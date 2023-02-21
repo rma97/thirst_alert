cd7530ffa9deb48cf9ff1ae5c8927a948a8df7c8
rma <rolfmandersen@hotmail.com>
2023-02-21 21:53:30 +0100

    vercel lets go

--- src/components/plantCard.tsx
+++ src/components/plantCard.tsx
@@ -1,5 +1,7 @@
-const PlantCard: React.FC = () => {
-  return <div></div>;
+import { type Plant } from "@prisma/client";
+
+const PlantCard: React.FC<{ plant: Plant }> = ({ plant }) => {
+  return <div>{plant.name}</div>;
 };
 
 export default PlantCard;
