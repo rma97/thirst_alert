919467f403dbd92974d3ad7ad738b38719afd6d9
rma <rolfmandersen@hotmail.com>
2023-02-21 22:32:46 +0100

    asd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = (props) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen bg-gradient-to-b from-[#eefbf3] to-[#136942]">
+      <main className="min-h-screen bg-gradient-to-b from-[#eefbf3] to-[#136941]">
         <Navbar />
         <AuthShowcase />
       </main>
